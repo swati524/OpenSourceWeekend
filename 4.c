afe2941c0d619005aab97240feee7e417bda25be
swati524 <31063947+swati524@users.noreply.github.com>
2017-12-10 12:26:49 +0530

    Updating Previous code
    Add some comments

--- 4.c
+++ 4.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
-void main(){
-	int num;
+int main(){
+	int num,num1;
 	printf("Enter number:");
-	scanf("%d",num);
-	int fact=1;
+	scanf("%d",&num);
+	int fact=1;  // initailize fact
+	num1=num; //to store the number
 	if(num==0){
 		fact==1;
 	}
 	else{
-		while(num!=1){
+		while(num!=1){              //continue loop till number becomes equal to 1
 		fact=fact*num;
 		num--;
 	}
 	}
 	
-	printf("Factorial of %d is %d",num,fact);
+	printf("Factorial of %d is %d",num1,fact);
+	return 0;
 }
